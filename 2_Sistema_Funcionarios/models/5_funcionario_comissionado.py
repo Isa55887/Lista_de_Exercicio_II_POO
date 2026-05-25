d43efda3369ba7443b86fb4d7190ee363f62837b
Isa55887 <ibeca4874@gmail.com>
2026-05-24 23:22:52 -0400

    Create 5_funcionario_comissionado.py

---
+++ 2_Sistema_Funcionarios/models/5_funcionario_comissionado.py
@@ -0,0 +1,8 @@
+class FuncionarioComissado(Funcionario):
+    def __init__(self, nome, cpf, total_vendas, percentual_comissao):
+        super().__init__(nome, cpf)
+        self.total_vendas = total_vendas
+        self.percentual_comissao = percentual_comissao
+        
+    def calcular_pagamento(self):
+        return self.total_vendas * (self.percentual_comissao / 100)
