a069cbe8461a2d1da873fc88752acccb8d66893d
Isa55887 <ibeca4874@gmail.com>
2026-05-24 23:51:43 -0400

    Create Impressao.py

---
+++ 4_Sistema_Impressao/Impressao.py
@@ -0,0 +1,31 @@
+from typing import Protocol
+
+class Imprimivel(Protocol):
+    def imprimir(self) -> None:
+        ...
+
+class Boleto:
+    def __init__(self, codigo: str, valor: float):
+        self.codigo = codigo
+        self.valor = valor
+
+    def imprimir(self) -> None:
+        print(f"boleto - codigo: {self.codigo}, valor R${self.valor}")
+
+class Etiqueta:
+    def __init__(self, destinatario: str, endereco: str):
+        self.destinatario = destinatario
+        self.endereco = endereco
+
+    def imprimir(self) -> None:
+        print(f"etiqueta - destinatario: {self.destinatario}, endereco:{self.endereco}")
+
+class RelatorioSimples:
+    def __init__(self, titulo: str):
+        self.titulo = titulo
+
+    def imprimir(self) -> None:
+        print(f"relatorio: {self.titulo}")
+
+def processar_impressao(item: Imprimivel) -> None:
+    item.imprimir()
